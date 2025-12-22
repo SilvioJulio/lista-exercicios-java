f1e533332f4fc6c2828b206c44a6aeda82969bd4
Silvio Júlio dos Santos <silvio.santos@dbserver.com.br>
2025-12-21 23:58:09 -0300

    Questão 16 = Programa: controle de empréstimo, concluída.

---
+++ exercicio/src/main/java/org/academiadb/questao16/Livro.java
@@ -0,0 +1,38 @@
+package org.academiadb.questao16;
+
+public class Livro {
+    private static int  ccntroleResgistroLivro = 100;
+    private final int numeroRegistro ;
+    String titulo;
+    String autor;
+    String genero;
+
+    public Livro(String titulo, String autor, String genero) {
+
+        if (titulo == null || titulo.isBlank()) throw new IllegalArgumentException("O campo titulo, não pode ser vazio");
+        if (autor == null || autor.isBlank()) throw new IllegalArgumentException("O campo autor, não pode ser vazio ");
+
+        this.numeroRegistro = ccntroleResgistroLivro++;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getNumeroRegistro() {
+        return numeroRegistro;
+     }
+}
---
+++ exercicio/src/test/java/PessoaTest.java
@@ -0,0 +1,26 @@
+import org.academiadb.questao16.Pessoa;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PessoaTest {
+
+    @Test
+    void validarLeitor() {
+        Pessoa leitor = new Pessoa("Luiz Silva", 23, "M", "(71) 998914562", "Rua João Silva, 34");
+        assertEquals("Luiz Silva", leitor.getNome());
+        assertEquals("(71) 99891456", leitor.getContato());
+
+
+    }
+
+    @Test
+    void verificarContatoLeitor() {
+        Pessoa leitor = new Pessoa("Luiz Silva", 23, "M", "(71) 99891-4562", "Rua João Silva, 34");
+        assertEquals("(71) 99891-4562", leitor.getContato());
+
+
+    }
+
+
+}
